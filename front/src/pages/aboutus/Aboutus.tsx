6df61b05498d019ebed21f2b514664b5192c09ad
Taewoong <aipooh8882@naver.com>
2021-10-08 21:35:37 +0900

    about us completed the overal layout

--- front/src/pages/aboutus/Aboutus.tsx
+++ front/src/pages/aboutus/Aboutus.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import Header from "@components/header/Header";
 import Footer from '@components/footer/Footer';
-import {Container, ContentBody, FirstContent, ImageFir, SecContentBody} from "@pages/aboutus/style";
-import image from "img/aboutus/background3.jpeg";
+import {
+    Container, FifthContentBody,
+    FirContentBody,
+    FirstContent,
+    FourthContentBody,
+    ImageFir,
+    SecContentBody,
+    ThrContentBody,
+    ContactUs, ContentOfContactUs, TableOfContactUs, SixthContentBody,
+} from "@pages/aboutus/style";
 
 const Aboutus = () => {
 
@@ -10,18 +18,68 @@ const Aboutus = () => {
         // <Container style={{backgroundImage: `url(${process.env.PUBLIC_URL + '/img/mainpage/background1.jpg'})`}}>
         <Container >
             <Header/>
-                <ContentBody>
+                <FirContentBody>
                     <ImageFir style={{backgroundImage: `url(${process.env.PUBLIC_URL + '/img/aboutus/background6.jpeg'})`}}/>
                     <FirstContent>
                         <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h4>
                         <h4>Nullam id augue ut justo imperdiet aliquet non quis nibh.</h4>
                         <h4>Sed lacinia lacus in tellus hendrerit varius eu eget sem.</h4>
                     </FirstContent>
-                </ContentBody>
+                </FirContentBody>
                 <SecContentBody>
                     asa
                 </SecContentBody>
-            {/*<Footer/>*/}
+                <ThrContentBody>
+                    팀원 소개
+                </ThrContentBody>
+                <ContactUs>
+                    <div>
+                        HOW CAN WE HELP
+                        <span> </span>
+                        <span style={{textDecoration: "underline"}}>YOU</span>
+                        ?
+                    </div>
+                    <ContentOfContactUs>
+                        Do you have an audience you need to persuade to take action? Persuading readers to take action is why we started a direct response copywriting agency, so we would love to hear more about what you would like to achieve with your marketing. Please send us a message in the form below, <a href="mailto:aipooh8882@naver.com">email us</a> or call us on <a href="tel:021234567">02 123 4567</a>.
+                    </ContentOfContactUs>
+                    <form>
+                        <TableOfContactUs>
+                            <tr>
+                                <td><input name="input_1" type="text" value="" className="medium"
+                                           placeholder="First Name*" aria-required="true" aria-invalid="false"/></td>
+                                <td><input name="input_2" type="text" value="" className="medium"
+                                           placeholder="Last Name*" aria-required="true" aria-invalid="false"/></td>
+                            </tr>
+                            <tr>
+                                <td><input name="input_3" type="text" value="" className="medium"
+                                           placeholder="Email*" aria-required="true" aria-invalid="false"/></td>
+                                <td><input name="input_4" type="text" value="" className="medium"
+                                           placeholder="Phone Number*" aria-required="true" aria-invalid="false"/></td>
+                            </tr>
+                            <tr>
+                                <td colSpan={2}><input name="input_5" type="text" value="" className="medium"
+                                                       placeholder="Company*" aria-required="true" aria-invalid="false"/></td>
+                            </tr>
+                            <tr>
+                                <td colSpan={2}><textarea name="input_6" className="textarea medium"
+                                                          placeholder="Message*" aria-required="true"
+                                                          aria-invalid="false" rows={10} cols={50}></textarea></td>
+                            </tr>
+                        </TableOfContactUs>
+                    </form>
+
+                </ContactUs>
+                <FourthContentBody>
+                    협업한? 브랜드 소개
+                </FourthContentBody>
+                <FifthContentBody>
+                    {/* eslint-disable-next-line react/no-unescaped-entities */}
+                    <div>“Love, don't fight. But fight for what you love.” <br/><br/><br/>
+                        ― Richie Norton</div>
+                </FifthContentBody>
+                <SixthContentBody>
+
+                </SixthContentBody>
         </Container>
     )
 
--- front/src/pages/aboutus/style.tsx
+++ front/src/pages/aboutus/style.tsx
@@ -5,9 +5,10 @@ export const Container = styled.div`
   background-repeat: no-repeat;
   background-attachment: fixed;
   background-size: cover;
+  font-family: "Aron Grotesque Light", Verdana, Arial, sans-serif;
 `;
 
-export const ContentBody = styled.div`
+export const FirContentBody = styled.div`
   //height: 100%;  
   //background-color: #02FFD5;
   padding-top: 5%;
@@ -20,11 +21,111 @@ export const ContentBody = styled.div`
 export const SecContentBody = styled.div`
   background-color: #282f3b;
   width:100%;
-  height: 50%;
+  height: 40%;
   margin-left: auto;
   margin-right: auto;
   align-items: center;
 `;
+
+export const ThrContentBody = styled.div`
+  background-color: rgba(230,232,234,0.7);
+  width:100%;
+  height: 60%;
+  margin-left: auto;
+  margin-right: auto;
+  align-items: center;
+`;
+
+export const ContactUs = styled.div`
+  background-color: white;
+  width:70%;
+  height: 60%;
+  margin-left: auto;
+  margin-right: auto;
+  align-items: center;
+  padding-top: 10vh;
+  & > div {
+    //background-color: #3d943d;
+    font-family: "Aron Grotesque Light", Verdana, Arial, sans-serif;
+    font-weight: 400;
+    font-size: 45px;
+    line-height: 1.2;
+    letter-spacing: 4px;
+    text-align: center;
+    width: 100%;
+    height: 10%;
+  }
+`;
+
+export const ContentOfContactUs = styled.p`
+  font-size: 20px;
+  line-height: 32px;
+  font-family: AppleGothic;
+`;
+
+export const TableOfContactUs = styled.table`
+  padding-top: 7%;
+  //border: 1px solid #444444;
+  width: 100%;
+  border-spacing: 1vw;
+  & > th, td {
+    //border: 1px solid #444444;
+      & > input {
+        background-color: gainsboro;
+        color:white;
+        width: 99%;
+        height: 60px;
+        font-size: 15px;
+        font-weight: 700;
+        border: 0;
+        outline: 0;
+      }
+    & > textarea {
+      background-color: gainsboro;
+      color:white;
+      width: 99%;
+      height: 200px;
+      font-size: 15px;
+      font-weight: 700;
+      border: 0;
+      outline: 0;
+    }
+    input::placeholder { font-size: 180%;  color: #aaaaaa }
+    textarea::placeholder { font-size: 210%;  color: #aaaaaa }
+  }
+`;
+
+export const FourthContentBody = styled.div`
+  background-color: #282f3b;
+  width:100%;
+  height: 16%;
+  margin-left: auto;
+  margin-right: auto;
+  align-items: center;
+`;
+
+export const FifthContentBody = styled.div`  // 명언
+  background-color: #bf9786;
+  width:100%;
+  height: 17%;
+  margin-left: auto;
+  margin-right: auto;
+  align-items: center;
+  & > div {
+    text-align: center;
+    padding-top: 8.5%;
+  }
+`;
+
+export const SixthContentBody = styled.div`
+  background-color: #282f3b;
+  width:100%;
+  height: 20%;
+  margin-left: auto;
+  margin-right: auto;
+  align-items: center;
+`;
+
 
 export const FirstContent = styled.div`
   margin: 0;
