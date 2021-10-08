e982e409ff1cd2f7c9558288c88ae42d35d1cc3b
Taewoong <aipooh8882@naver.com>
2021-10-08 19:39:28 +0900

    still working on the about us page. I stretched the whole size of it and am gonna keep filling it with contents

--- front/src/pages/aboutus/Aboutus.tsx
+++ front/src/pages/aboutus/Aboutus.tsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import Header from "@components/header/Header";
 import Footer from '@components/footer/Footer';
-import {Container, ContentBody, FirstContent} from "@pages/aboutus/style";
+import {Container, ContentBody, FirstContent, ImageFir, SecContentBody} from "@pages/aboutus/style";
+import image from "img/aboutus/background3.jpeg";
 
 const Aboutus = () => {
 
     return (
-        <Container style={{backgroundImage: `url(${process.env.PUBLIC_URL + '/img/mainpage/background1.jpg'})`}}>
+        // <Container style={{backgroundImage: `url(${process.env.PUBLIC_URL + '/img/mainpage/background1.jpg'})`}}>
+        <Container >
             <Header/>
                 <ContentBody>
+                    <ImageFir style={{backgroundImage: `url(${process.env.PUBLIC_URL + '/img/aboutus/background6.jpeg'})`}}/>
                     <FirstContent>
                         <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h4>
                         <h4>Nullam id augue ut justo imperdiet aliquet non quis nibh.</h4>
                         <h4>Sed lacinia lacus in tellus hendrerit varius eu eget sem.</h4>
                     </FirstContent>
                 </ContentBody>
-            <Footer/>
+                <SecContentBody>
+                    asa
+                </SecContentBody>
+            {/*<Footer/>*/}
         </Container>
     )
 
--- front/src/pages/aboutus/style.tsx
+++ front/src/pages/aboutus/style.tsx
@@ -1,22 +1,45 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
-  height: 100%;
+  height: 200%;
   background-repeat: no-repeat;
   background-attachment: fixed;
   background-size: cover;
 `;
 
 export const ContentBody = styled.div`
-  height: 200%;
+  //height: 100%;  
+  //background-color: #02FFD5;
+  padding-top: 5%;
+  width:70%;
+  margin-left: auto;
+  margin-right: auto;
+  align-items: center;
+`;
+
+export const SecContentBody = styled.div`
+  background-color: #282f3b;
+  width:100%;
+  height: 50%;
+  margin-left: auto;
+  margin-right: auto;
+  align-items: center;
 `;
 
 export const FirstContent = styled.div`
   margin: 0;
-  padding: 13% 17% 0;
+  padding: 0% 35% 0;
   font-size: 35px;
   line-height: 95%;
-  width: 50%;
+  width: 30%;
   font-weight: 500;
   background-color: transparent;
+  text-align: center;
+`;
+
+export const ImageFir = styled.div`
+  height: 300px;
+  width: 100%;
+  background-repeat:no-repeat;
+  background-position:center top;
 `;
