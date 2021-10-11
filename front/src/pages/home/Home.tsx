249f27a5b66deca50a0d44279b2e43e0c737e07d
joo <pingrae@me.com>
2021-10-11 17:51:34 +0900

    change main page theme

--- front/src/pages/home/Home.tsx
+++ front/src/pages/home/Home.tsx
@@ -4,13 +4,14 @@ import Header from '@components/header/Header';
 import Footer from '@components/footer/Footer';
 import { GiClick } from "react-icons/gi";
 import '@pages/home/Home.css'
+import { FaCentercode } from 'react-icons/fa';
 
 const Home = () => {
     return (
-        <div className="content" style={{backgroundImage: `url(${process.env.PUBLIC_URL + '/img/mainpage/background4.jpg'})`}}>
+        <div className="content" style={{backgroundImage: `url(${process.env.PUBLIC_URL + '/img/mainpage/background0.jpeg'})`, backgroundPosition: 'center'}}>
             <Header/>
             <div className="contentBody">
-                <h1 className="contentTitle">Immutable Signature<br/>on Blockchain</h1>
+                <h1 className="contentTitle">Love Chain</h1>
                 <h3 className="contentSubTitle">Your love that won&apos;t change, your last will for your family, what you want to say to the world...<br/>Leave your message that you want to record anything forever.</h3>
                 <Link to="/theme/choose">
                     <button className="goService">Leave your Message! <GiClick /></button>
